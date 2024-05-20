61d709ac58652f15b43ef0ae8ad7ef4846f94d38
KamilSikora <kamilsikora1316@gmail.com>
2024-05-20 20:28:24 +0200

    added update procedures

--- sWMS/SQL/Procedures.sql
+++ sWMS/SQL/Procedures.sql
@@ -214,7 +214,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Tworzenie nowej kartoteki towarowej
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewArticle
+CREATE OR ALTER PROCEDURE sWMS.CreateNewArticle
 	@Art_Type int,
 	@Art_No int,
 	@Art_Code varchar(100),
@@ -255,7 +255,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Tworzenie nowej klasy atrybutu
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewClassOfAttribute
+CREATE OR ALTER PROCEDURE sWMS.CreateNewClassOfAttribute
 	@AtC_Type int,
 	@AtC_No int,
 	@AtC_Name varchar(100),
@@ -289,7 +289,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Tworzenie nowego atrybutu
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewAttribute
+CREATE OR ALTER PROCEDURE sWMS.CreateNewAttribute
 	@Attr_Type int,
 	@Attr_No int,
 	@Attr_Object_Id int,
@@ -338,7 +338,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Modyfikacja konfiguracji
 -- =============================================
-CREATE PROCEDURE sWMS.UpdateConfig
+CREATE OR ALTER PROCEDURE sWMS.UpdateConfig
 	@Conf_Id int,
 	@Conf_Value varchar(100)
 AS
@@ -359,7 +359,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Dodawanie nowego kontrahenta
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewContractor
+CREATE OR ALTER PROCEDURE sWMS.CreateNewContractor
 	@Con_Type int,
 	@Con_No int,
 	@Con_Code varchar(100),
@@ -433,7 +433,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Dodawanie nowej jednostki
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewUnit
+CREATE OR ALTER PROCEDURE sWMS.CreateNewUnit
 	@Unit_Type int,
 	@Unit_No int,
 	@Unit_Name varchar(100),
@@ -480,7 +480,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Dodawanie nowej nazwy niestandardowej
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewCustomName
+CREATE OR ALTER PROCEDURE sWMS.CreateNewCustomName
 	@Cun_Type int,
 	@Cun_No int,
 	@Cun_Name varchar(100)
@@ -512,7 +512,7 @@ GO
 -- Create date: 19.05.2024
 -- Description:	Dodawanie nowego magazynu
 -- =============================================
-CREATE PROCEDURE sWMS.CreateNewWarehouse
+CREATE OR ALTER PROCEDURE sWMS.CreateNewWarehouse
 	@Wh_Code varchar(100),
 	@Wh_Name varchar(100),
 	@Wh_Country varchar(100) = null,
@@ -553,7 +553,7 @@ GO
 -- Create date: 20.05.2024
 -- Description:	Usuwanie pozycji z dokumentu
 -- =============================================
-CREATE PROCEDURE sWMS.RemoveItemFromDocument
+CREATE OR ALTER PROCEDURE sWMS.RemoveItemFromDocument
 	@Doc_Id int,
 	@Doc_Type int,
 	@It_No int
@@ -725,5 +725,383 @@ BEGIN
 	where Unit_Id = @Unit_Id
 		and Unit_Type = @Unit_Type
 		and Unit_No = @Unit_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja towaru
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateArticle
+	@Art_Id int
+	,@Art_Type int
+	,@Art_No int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    delete from sWMS.Articles
+	where Art_Id = @Art_Id
+		and Art_Type = @Art_Type
+		and Art_No = @Art_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja towaru
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateArticle
+	@Art_Id int
+	,@Art_Type int
+	,@Art_No int
+	,@Art_Code varchar(100)
+	,@Art_Name varchar(100)
+	,@Art_Default_UnitId int
+	,@Art_CreationDate datetime
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    update sWMS.Articles
+	set	Art_Code = @Art_Code,
+	Art_Name = @Art_Name,
+	Art_Default_UnitId = @Art_Default_UnitId,
+	Art_CreationDate = @Art_CreationDate
+	where Art_Id = @Art_Id
+		and Art_Type = @Art_Type
+		and Art_No = @Art_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja kontrahenta
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateContractor
+	@Con_Id int
+	,@Con_Type int
+	,@Con_No int
+	,@Con_Code varchar(100)
+	,@Con_FullName varchar(100)
+	,@Con_Country varchar(100) = null
+	,@Con_City varchar(100) = null
+	,@Con_Street varchar(100) = null
+	,@Con_Postal varchar(100) = null
+	,@Con_Code_Cun_Id int = null
+	,@Con_Code_Cun_Type int = null
+	,@Con_Code_Cun_No int = null
+	,@Con_Name_Cun_Id int = null
+	,@Con_Name_Cun_Type int = null
+	,@Con_Name_Cun_No int = null
+	,@Con_Logo_BinD_Id int = null
+	,@Con_Logo_BinD_Type int = null
+	,@Con_Logo_BinD_No int = null
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    update sWMS.Contractors
+	set	Con_Code = @Con_Code
+	,Con_FullName = @Con_FullName
+	,Con_Country = @Con_Country 
+	,Con_City = @Con_City 
+	,Con_Street = @Con_Street 
+	,Con_Postal = @Con_Postal 
+	,Con_Code_Cun_Id = @Con_Code_Cun_Id 
+	,Con_Code_Cun_Type = @Con_Code_Cun_Type 
+	,Con_Code_Cun_No = @Con_Code_Cun_No 
+	,Con_Name_Cun_Id = @Con_Name_Cun_Id 
+	,Con_Name_Cun_Type = @Con_Name_Cun_Type 
+	,Con_Name_Cun_No = @Con_Name_Cun_No 
+	,Con_Logo_BinD_Id = @Con_Logo_BinD_Id 
+	,Con_Logo_BinD_Type = @Con_Logo_BinD_Type 
+	,Con_Logo_BinD_No = @Con_Logo_BinD_No 
+	where Con_Id = @Con_Id
+		and Con_Type = @Con_Type
+		and Con_No = @Con_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja magazynu
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateWarehouse
+	@Wh_Id int,
+	@Wh_Code varchar(100),
+	@Wh_Name varchar(100),
+	@Wh_Country varchar(100) = null,
+	@Wh_City varchar(100) = null,
+	@Wh_Street varchar(100) = null,
+	@Wh_Postal varchar(100) = null
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.Warehouses
+	set Wh_Code = @Wh_Code,
+	Wh_Name = @Wh_Name,
+	Wh_Country = @Wh_Country,
+	Wh_City = @Wh_City,
+	Wh_Street = @Wh_Street,
+	Wh_Postal = @Wh_Postal
+	where Wh_Id = @Wh_Id 
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja partii
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateArticlesBatches
+	@ArB_Id int,
+	@ArB_Type int,
+	@ArB_No int,
+	@ArB_Code varchar(100),
+	@ArB_Name varchar(100),
+	@ArB_Quantity decimal(14,9),
+	@ArB_Unit_Id int,
+	@ArB_Unit_Type int,
+	@ArB_Unit_No int,
+	@ArB_Secondary_Unit_Id int,
+	@ArB_Secondary_Unit_Type int,
+	@ArB_Secondary_Unit_No int,
+	@ArB_Art_Id int,
+	@ArB_Art_Type int,
+	@ArB_Art_No int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.ArticlesBatches
+	set ArB_Code = @ArB_Code,
+		ArB_Name = @ArB_Name,
+		ArB_Quantity = @ArB_Quantity,
+		ArB_Unit_Id = @ArB_Unit_Id,
+		ArB_Unit_Type = @ArB_Unit_Type,
+		ArB_Unit_No = @ArB_Unit_No,
+		ArB_Secondary_Unit_Id = @ArB_Secondary_Unit_Id,
+		ArB_Secondary_Unit_Type = @ArB_Secondary_Unit_Type,
+		ArB_Secondary_Unit_No = @ArB_Secondary_Unit_No,
+		ArB_Art_Id = @ArB_Art_Id,
+		ArB_Art_Type = @ArB_Art_Type,
+		ArB_Art_No = @ArB_Art_No
+	where ArB_Id = @ArB_Id
+		and ArB_Type = @ArB_Type
+		and ArB_No = @ArB_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja nazw
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateCustomName
+	@Cun_Id int,
+	@Cun_Type int,
+	@Cun_No int,
+	@Cun_Name varchar(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.CustomNames
+	set Cun_Name = @Cun_Name 
+	where Cun_Id = @Cun_Id
+		and Cun_Type = @Cun_Type
+		and Cun_No = @Cun_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja atrybutów
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateAttributes
+	@Attr_Id int,
+	@Attr_Type int,
+	@Attr_No int,
+	@Attr_Value varchar(255)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.Attributes
+	set Attr_Value = @Attr_Value
+	where Attr_Id = @Attr_Id
+		and Attr_Type = @Attr_Type
+		and Attr_No = @Attr_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja klas atrybutów
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateClassOfAttribute
+	@AtC_Id int,
+	@AtC_Type int,
+	@AtC_No int,
+	@AtC_Name varchar(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.AttrClasses
+	set AtC_Name = @AtC_Name
+	where AtC_Id = @AtC_Id
+		and AtC_Type = @AtC_Type
+		and AtC_No = @AtC_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja dokumentów
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateDocument
+	@Doc_Id int,
+	@Doc_Type int,
+	@Doc_NumberString varchar(100),
+	@Doc_Number int,
+	@Doc_Month int,
+	@Doc_Year int,
+	@Doc_Series varchar(5),
+	@Doc_CreationDate datetime,
+	@Doc_CompletionDate datetime
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.Documents
+	set Doc_NumberString = @Doc_NumberString,
+	Doc_Number = @Doc_Number,
+	Doc_Month = @Doc_Month,
+	Doc_Year = @Doc_Year,
+	Doc_Series = @Doc_Series,
+	Doc_CreationDate = @Doc_CreationDate,
+	Doc_CompletionDate = @Doc_CompletionDate
+	where Doc_Id = @Doc_Id
+		and Doc_Type = @Doc_Type
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja jednostek
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateUnit
+	@Unit_Id int,
+	@Unit_Type int,
+	@Unit_No int,
+	@Unit_Name varchar(100),
+	@Unit_Dividend decimal(14,9),
+	@Unit_Divisor decimal(14,9),
+	@Unit_AttachedTo_Art_Id int,
+	@Unit_AttachedTo_Art_Type int,
+	@Unit_AttachedTo_Art_No int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.Units
+	set Unit_Name = @Unit_Name,
+	Unit_Dividend = @Unit_Dividend,
+	Unit_Divisor = @Unit_Divisor,
+	Unit_AttachedTo_Art_Id = @Unit_AttachedTo_Art_Id,
+	Unit_AttachedTo_Art_Type = @Unit_AttachedTo_Art_Type,
+	Unit_AttachedTo_Art_No = @Unit_AttachedTo_Art_No
+	where Unit_Id = @Unit_Id
+		and Unit_Type = @Unit_Type
+		and Unit_No = @Unit_No
+END
+GO
+
+-- =============================================
+-- Author:		Kamil Sikora
+-- Create date: 20.05.2024
+-- Description:	Modyfikacja pozycji dokumentu
+-- =============================================
+CREATE OR ALTER PROCEDURE sWMS.UpdateDocumentItem
+	@Doc_Id int,
+	@Doc_Type int,
+	@It_No int,
+	@IT_Code varchar(100),
+	@IT_Name varchar(100),
+	@IT_Quantity decimal(14,9),
+	@IT_Description varchar(255),
+	@IT_CompletionDate datetime,
+	@IT_Unit_Id int,
+	@IT_Unit_Type int,
+	@IT_Unit_No int,
+	@IT_Secondary_Unit_Id int,
+	@IT_Secondary_Unit_Type int,
+	@IT_Secondary_Unit_No int,
+	@IT_Code_Cun_Id int,
+	@IT_Code_Cun_Type int,
+	@IT_Code_Cun_No int,
+	@IT_Name_Cun_Id int,
+	@IT_Name_Cun_Type int,
+	@IT_Name_Cun_No int,
+	@IT_Src_Wh_Id int,
+	@IT_Current_Wh_Id int,
+	@IT_Dst_Wh_Id int,
+	@IT_Art_Id int,
+	@IT_Art_Type int,
+	@IT_Art_No int,
+	@IT_ArB_Id int,
+	@IT_Arb_Type int,
+	@IT_Arb_No int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    update sWMS.Items
+	set IT_Code = @IT_Code,
+	IT_Name = @IT_Name,
+	IT_Quantity = @IT_Quantity,
+	IT_Description = @IT_Description,
+	IT_CompletionDate = @IT_CompletionDate,
+	IT_Unit_Id = @IT_Unit_Id,
+	IT_Unit_Type = @IT_Unit_Type,
+	IT_Unit_No = @IT_Unit_No,
+	IT_Secondary_Unit_Id = @IT_Secondary_Unit_Id,
+	IT_Secondary_Unit_Type = @IT_Secondary_Unit_Type,
+	IT_Secondary_Unit_No = @IT_Secondary_Unit_No,
+	IT_Code_Cun_Id = @IT_Code_Cun_Id,
+	IT_Code_Cun_Type = @IT_Code_Cun_Type,
+	IT_Code_Cun_No = @IT_Code_Cun_No,
+	IT_Name_Cun_Id = @IT_Name_Cun_Id,
+	IT_Name_Cun_Type = @IT_Name_Cun_Type,
+	IT_Name_Cun_No = @IT_Name_Cun_No,
+	IT_Src_Wh_Id = @IT_Src_Wh_Id,
+	IT_Current_Wh_Id = @IT_Current_Wh_Id,
+	IT_Dst_Wh_Id = @IT_Dst_Wh_Id,
+	IT_Art_Id = @IT_Art_Id,
+	IT_Art_Type = @IT_Art_Type,
+	IT_Art_No = @IT_Art_No,
+	IT_ArB_Id = @IT_ArB_Id,
+	IT_Arb_Type = @IT_Arb_Type,
+	IT_Arb_No = @IT_Arb_No
+	where IT_Doc_Id = @Doc_Id
+		and It_Doc_Type = @Doc_Type
+		and It_No = @It_No
 END
 GO
