25ec63a2c69e3e05afd0c84bb0046505a948b9b7
KamilSikora <kamilsikora1316@gmail.com>
2024-05-23 12:37:59 +0200

    pokombinowac jeszcze z oknami

--- sWMS/Windows/AllDocumentsList.xaml.cs
+++ sWMS/Windows/AllDocumentsList.xaml.cs
@@ -1,5 +1,4 @@
-﻿using sWMS.Windows;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace sWMS
+namespace sWMS.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DocumentsList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AllDocumentsList : Window
     {
-        public MainWindow()
+        public AllDocumentsList()
         {
-            WarehousesList warehousesList = new WarehousesList();
-            warehousesList.ShowDialog();
-            //InitializeComponent();
+            InitializeComponent();
         }
     }
 }
